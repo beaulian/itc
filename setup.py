45151391103652a7944a4998615feab2cad5cdb9
shenaishiren <gjw.jesus@qq.com>
2015-06-04 21:49:22 +0800

    set charset

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#-*-coding:utf-8-*-
 from setuptools import setup, find_packages
 import sys,os
 
@@ -8,13 +9,12 @@ setup(name='gavin',
       keywords='python iciba dictionary terminal',
       author='gavin',
       author_email='870402916@qq.com',
-      url='https://github.com/Flowerowl/ici',
+      url='https://github.com/shenaishiren/itc',
       license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-      	'xml',
         'termcolor',
       ],
       entry_points={
